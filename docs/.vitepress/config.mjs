d0b84b3e37f4d99a340e0aaf823ed9b286e8619f
FourierDrive <tt14283065@gmail.com>
2026-09-04 16:48:10 +0800

    Create config.mjs

---
+++ docs/.vitepress/config.mjs
@@ -0,0 +1,17 @@
+export default {
+  title: 'wiki',
+  description: 'record',
+  themeConfig: {
+    nav: [
+      { text: 'main', link: '/' }
+    ],
+    sidebar: [
+      {
+        text: 'dir',
+        items: [
+          { text: 'welcome', link: '/' }
+        ]
+      }
+    ]
+  }
+}
